055e05e507a8d608a3572ce1ebd3fd14402fb02e
ts8zs <ts8zs@qq.com>
2016-10-11 20:02:15 +0800

    _(:3」∠)_

--- BiliLiveMedalCounter.py
+++ BiliLiveMedalCounter.py
@@ -7,29 +7,33 @@ import urllib
 import json
 import threading
 
-##todo: 断点续查 多线程排序 免查房间 编码头问题
+##todo: 断点续查 排序 已有房间免查 编码头
 
-def PaoXunZhang(roomid):
+logput = open('log.txt', 'w+')
+output = open('Medal.txt', 'w+')
+
+def CheckRoom(roomid):
     url = 'http://live.bilibili.com/liveact/ajaxGetMedalRankList?roomid='+ str(roomid)
-    req = requests.request(url=url,method='GET')
+    try:
+        req = requests.request(url=url,method='GET')
+    except:
+        CheckRoom(roomid)
+        logput.write(roomid+'出错  ')
+        logput.flush()
     if(req):
         req=json.loads(req.content.decode())
-        print(str(roomid))
         try:
             if(req['data']['list'][0]['medalName']):
                 print(req['data']['list'][0]['medalName'])
                 try:
                     output.write(req['data']['list'][0]['medalName']+','+str(roomid)+'   ')
                     output.flush()
-                except Exception as e:
-                    print(e);
-                    logput.write(e)
-                    logput.flush()
+                except:
+                    pass
         except:
             pass
 
-logput=open('log.txt', 'w+')
-output = open('PaoXunZhang.txt', 'w+')
+
 
 def main():
     ##创建线程池
@@ -40,9 +44,9 @@ def main():
 
     x=1000 ##起始房间号 低于1000均为假房间号
     while x<1000000: ##终止房间号
-        for i in range(0,threadsNum):
+        for i in range(0,threadsNum): ##反复遍历线程
             if threads[i].isAlive()==False:
-                threads[i]=threading.Thread(target=PaoXunZhang,args=(x,))
+                threads[i]=threading.Thread(target=CheckRoom,args=(x,))
                 threads[i].start()
                 x+=1
     sleep(30) ##超时等待
